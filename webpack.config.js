5f999ad0c93139a4cd50fe73f246ff4979472688
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-09 10:19:57 +0000

    feat: Add webpack config file for production

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/frontend/packs/index.jsx',
+  output: {
+    filename: './public/packs',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
