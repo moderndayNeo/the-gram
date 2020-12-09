27c3f293b8381b045b0dcb4f1a8f71fd0322c3d8
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-09 10:08:55 +0000

    feat: Render the root component in index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :login_user!, :logout_current_user!
+  helper_method :current_user, :login_user!, :logout_current_user!, :logged_in?
 
   def current_user
     return nil if session[:session_token].nil?
--- app/frontend/packs/index.jsx
+++ app/frontend/packs/index.jsx
@@ -1,22 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from '../src/root'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+  const root = document.getElementById('root');
+
+  ReactDOM.render(<Root />, root);
+
+});
