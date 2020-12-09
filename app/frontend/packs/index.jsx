2e950a3238f54778854d564d9506eb90deb494d4
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-09 12:29:33 +0000

    feat: Pass the store to the root component

--- app/frontend/packs/index.jsx
+++ app/frontend/packs/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from '../src/root'
+import Root from '../src/root';
+import configureStore from '../src/redux/store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
 
-  ReactDOM.render(<Root />, root);
+  ReactDOM.render(<Root store={store} />, root);
 
 });
