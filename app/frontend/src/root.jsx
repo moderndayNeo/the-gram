6b2f51f77e8f4126cf17c666991a8108af61c423
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-09 10:29:08 +0000

    chore: Install redux, routing, axios node packages

--- app/frontend/src/root.jsx
+++ app/frontend/src/root.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Root() {
     return (
         <div>
-            Root Component
+            Hello Adam, the-gram is live!
         </div>
     );
 }
