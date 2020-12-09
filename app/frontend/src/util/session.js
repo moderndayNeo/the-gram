91db15c672a984da8ff1a4bff60eea9224e010a2
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-09 11:37:45 +0000

    feat: Create axios helpers for signup/login/logout

--- app/frontend/src/util/session.js
+++ app/frontend/src/util/session.js
@@ -1,1 +1,7 @@
-// axios request to sign in user
+import axiosRequest from './axios_request'
+
+export const createUser = (user) => axiosRequest('post', '/api/users', user)
+
+export const loginUser = (user) => axiosRequest('post', '/api/session', user)
+
+export const logoutUser = () => axiosRequest('delete', '/api/session')
