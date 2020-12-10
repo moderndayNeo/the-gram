41b24678cd88566a5d33180f0852d4c894e53769
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-10 15:16:03 +0000

    refactor: Only show logout button when signed in

--- app/frontend/src/components/app.jsx
+++ app/frontend/src/components/app.jsx
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
-import { logoutUser } from '../redux/actions/session_actions';
 import Home from './home/home';
-import { useDispatch } from 'react-redux';
-import { AuthRoute, ProtectedRoute } from '../util/route_util'
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 export default function App() {
 
-    const dispatch = useDispatch();
 
     return (
         <div>
             <Link to="/signup">Sign Up</Link>
             <Link to="/login">Login</Link>
-            <button onClick={() => dispatch(logoutUser())}>Log Out</button>
-            <ProtectedRoute path="/" component={Home} />
+
+            <ProtectedRoute exact path="/" component={Home} />
             <AuthRoute exact path="/signup" component={SignupContainer} />
             <AuthRoute exact path="/login" component={LoginContainer} />
         </div>
--- app/frontend/src/components/home/home.jsx
+++ app/frontend/src/components/home/home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { logoutUser } from '../../redux/actions/session_actions'
+import { useDispatch } from 'react-redux';
+
 
 export default function Home() {
+    const dispatch = useDispatch();
 
     const name = useSelector(state => state.session.name)
     const id = useSelector(state => state.session.id)
@@ -10,6 +14,7 @@ export default function Home() {
         <div>
             Home
             <h3>Welcome, {name}!</h3>
+            <button onClick={() => dispatch(logoutUser())}>Log Out</button>
         </div>
     );
 }
