b74823e2dd88bf53080e00ddc8b266224a4e1842
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-10 16:26:05 +0000

    refactor: Redirect to signup if user not logged in

--- app/frontend/src/util/route_util.js
+++ app/frontend/src/util/route_util.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Redirect, Route, withRouter } from 'react-router-dom';
+import React from 'react'
+import { connect } from 'react-redux'
+import { Redirect, Route, withRouter } from 'react-router-dom'
 
-const mapStateToProps = state => ({
-    loggedIn: Boolean(state.session.id)
-});
+const mapStateToProps = (state) => ({
+    loggedIn: Boolean(state.session.id),
+})
 
-const Auth = ({ component: Component, path, loggedIn }) => (
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
     <Route
+        exact={exact}
         path={path}
-        render={(props) => (
+        render={(props) =>
             loggedIn ? <Redirect to="/" /> : <Component {...props} />
-        )}
+        }
     />
-);
+)
 
-const Protected = ({ component: Component, path, loggedIn }) => (
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
     <Route
+        exact={exact}
         path={path}
-        render={props => (
+        render={(props) =>
             loggedIn ? <Component {...props} /> : <Redirect to="/signup" />
-        )}
+        }
     />
-);
+)
 
-export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+export const AuthRoute = withRouter(connect(mapStateToProps)(Auth))
 export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected))
