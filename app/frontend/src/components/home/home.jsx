879568a8dfb183c84f8254ebac7e634daa2a4f68
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-10 10:28:16 +0000

    refactor: Use useSelector to fetch user info from state

--- app/frontend/src/components/home/home.jsx
+++ app/frontend/src/components/home/home.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+    const name = useSelector(state => state.session.name);
+    const id = useSelector(state => state.session.id);
+
     return (
         <div>
             Home
-    {/* <h1>{window.currentUser.name}</h1> */}
+            <h3>{name}</h3>
+    <h3>{id}</h3>
         </div>
-    )
+    );
 }
 
 
 /*
 receiveCurrentUser action
-- sessions reducer responds to it 
+- sessions reducer responds to it
 - users reducer also responds to it, and stores the user info
-
-
-
 */
