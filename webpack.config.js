ec1ff8043c8e9323c4fdb427008a5b99b5000c67
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-10 17:34:34 +0000

    chore: Allow webpack to compile png files

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
-var path = require('path');
+var path = require('path')
 
 module.exports = {
-  entry: './app/frontend/packs/index.jsx',
-  output: {
-    filename: './public/packs',
-  },
-  module: {
-    rules: [
-      {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        },
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  }
-};
+    entry: './app/frontend/packs/index.jsx',
+    output: {
+        filename: './public/packs',
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react'],
+                    },
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*'],
+    },
+}
