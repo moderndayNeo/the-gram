0de567d154e6688d14fdde0221981deae8a9252b
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-11 15:43:13 +0000

    feat: Create LoginLink component

--- app/frontend/src/components/session/signup/SignupPage.jsx
+++ app/frontend/src/components/session/signup/SignupPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SignupForm from './signup_form_hook';
 import MainLogo from '../../shared/main_logo';
 import TermsAndConditions from './terms_and_conditions';
+import LoginLink from '../../shared/login_link'
 
 export default function SignupPage() {
     return (
@@ -11,6 +12,8 @@ export default function SignupPage() {
             <SignupForm />
 
         <TermsAndConditions />
+
+        <LoginLink />
 
 
         </div>
---
+++ app/frontend/src/components/shared/login_link.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function LoginLink() {
+    return (
+        <section className="login-link">
+            <p>Have an account?</p>
+            <Link to="">Log In</Link>
+        </section>
+    )
+}
