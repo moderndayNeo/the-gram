735e8d02f20d6eaa88fe0e56141603f4a0e2318f
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-12 12:31:48 +0000

    feat: Create, style OrSeparator component

--- app/frontend/src/components/session/login/login_page.jsx
+++ app/frontend/src/components/session/login/login_page.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
 import AppLinks from '../../shared/app_links';
-import LoginForm from './login_form'
-import MainLogo from '../../shared/main_logo'
-import SignupLoginLink from '../../shared/signup_login_link'
+import LoginForm from './login_form';
+import MainLogo from '../../shared/main_logo';
+import SignupLoginLink from '../../shared/signup_login_link';
+import GuestLoginButton from '../../shared/guest-login-button';
+import OrSeparator from '../../shared/or_separator';
 
 export default function LoginPage() {
     return (
         <div className="auth-page login-page">
             <MainLogo />
+            <GuestLoginButton />
+            <OrSeparator />
             <LoginForm />
             <SignupLoginLink text="Don't have an account?" linkText="Sign up" href="/signup" />
             <AppLinks />
         </div>
-    )
+    );
 }
---
+++ app/frontend/src/components/shared/or_separator.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default () => (
+    <div className="or-separator">
+        <div className="separator-line"></div>
+        <p>OR</p>
+        <div className="separator-line"></div>
+    </div>
+)
