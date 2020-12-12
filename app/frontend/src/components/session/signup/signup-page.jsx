44100e74c292161cc5864b0f58d8099a3fcaab5f
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-12 13:16:37 +0000

    feat: Add unfilled Instagram icons

--- app/frontend/src/components/session/signup/signup-page.jsx
+++ app/frontend/src/components/session/signup/signup-page.jsx
@@ -6,6 +6,10 @@ import AppLinks from '../../shared/app_links';
 import GuestLoginButton from '../../shared/guest-login-button';
 import SignupLoginLink from '../../shared/signup_login_link';
 import OrSeparator from '../../shared/or_separator';
+import UnfilledHeartIcon from './icons/unfilled-heart-icon.jsx';
+import UnfilledHomeIcon from './icons/unfilled-home-icon'
+import UnfilledCompassIcon from './icons/unfilled-compass-icon'
+import UnfilledNewPostIcon from './icons/unfilled-new-post-icon'
 
 export default function SignupPage() {
     return (
@@ -17,6 +21,10 @@ export default function SignupPage() {
             <SignupForm />
             <SignupTerms />
             <SignupLoginLink text="Have an account?" linkText="Log in" href="/login" />
+            <UnfilledHeartIcon />
+            <UnfilledHomeIcon />
+            <UnfilledCompassIcon />
+            <UnfilledNewPostIcon />
             <AppLinks />
         </div>
     );
