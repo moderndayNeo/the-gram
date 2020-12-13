17576bf86ea9efcedca088f4c3fcbf8459fc7f16
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-13 16:12:47 +0000

    chore: Remove redux dev tools comments

--- app/frontend/src/redux/store/store.js
+++ app/frontend/src/redux/store/store.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from './thunk'
 import rootReducer from '../reducers/root_reducer'
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const middlewares = [thunk]
 
 if (process.env.NODE_ENV !== 'production') {
@@ -11,15 +10,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const configureStore = (preloadedState = {}) =>
-    createStore(
-        rootReducer,
-        preloadedState,
-        applyMiddleware(...middlewares)
-        // composeEnhancers(applyMiddleware(...middlewares))
-    )
+    createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
 
 export default configureStore
-
-
-// "homepage":"/public/packs",
-// package json
