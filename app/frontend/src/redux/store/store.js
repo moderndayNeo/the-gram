4b5be5a38aea7f6e48f054ed4b60e070123f4616
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-13 15:49:42 +0000

    test: Remove redux devtools for deployment

--- app/frontend/src/redux/store/store.js
+++ app/frontend/src/redux/store/store.js
@@ -2,19 +2,24 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from './thunk'
 import rootReducer from '../reducers/root_reducer'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const middlewares = [thunk]
 
 if (process.env.NODE_ENV !== 'production') {
-  const { logger } = require('redux-logger')
-  middlewares.push(logger)
+    const { logger } = require('redux-logger')
+    middlewares.push(logger)
 }
 
 const configureStore = (preloadedState = {}) =>
     createStore(
         rootReducer,
         preloadedState,
-        composeEnhancers(applyMiddleware(...middlewares))
+        applyMiddleware(...middlewares)
+        // composeEnhancers(applyMiddleware(...middlewares))
     )
 
 export default configureStore
+
+
+// "homepage":"/public/packs",
+// package json
