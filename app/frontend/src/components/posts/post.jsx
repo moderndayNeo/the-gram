30e351760e98a81e7ffb7507fded9903373187ef
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-14 19:46:58 +0000

    feat: Display a Post's image on the feed

--- app/frontend/src/components/posts/post.jsx
+++ app/frontend/src/components/posts/post.jsx
@@ -3,28 +3,43 @@ import UserAvatar from '../shared/user_avatar';
 import icons from '../shared/icons/svg-icons';
 import { useSelector } from 'react-redux';
 
-export default function Post({ post: { id, author_id } }) {
+export default function Post({ post: { id, author_id, image_url } }) {
     const author = useSelector(state => state.entities.users[author_id]);
     const authorImage = author.image_url;
 
     return (
         <article className="post">
             <PostHeader author={author.username} authorImage={authorImage} />
-            {/* <PostImage id={id} imageUrl={image_url} /> */}
-            {/* <PostFooter/ > */}
+            <PostImage id={id} imageUrl={image_url} />
+            <PostFooter />
         </article>
     );
 }
 
 
 const PostHeader = ({ author, authorImage }) => {
-
     return (
         <header className="post-header">
             <UserAvatar imageUrl={authorImage} />
-
             <p>{author}</p>
             {icons.threeDots}
         </header>
+    );
+};
+
+const PostImage = ({ imageUrl }) => {
+
+    return (
+        <div className="image-container">
+            <img className="post-image" src={imageUrl || window.placeholderImg} alt="post image" />
+        </div>
+    );
+};
+
+const PostFooter = () => {
+    return (
+        <div className="post-footer">
+            Footer
+        </div>
     );
 };
