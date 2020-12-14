94a0c0db999491d4d0ea29cdf5bf0801e8c46907
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-14 15:06:42 +0000

    chore: Cleanup home component, remove unused code

--- app/frontend/src/components/home/home.jsx
+++ app/frontend/src/components/home/home.jsx
@@ -1,44 +1,21 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { logoutUser } from '../../redux/actions/session_actions';
-import { useDispatch } from 'react-redux';
-import ImageForm from '../user/image_form';
 import Feed from '../posts/feed';
-import PostForm from '../posts/post_form';
-import HomeTopNav from './home_top_nav'
-import BottomNav from '../shared/bottom_nav'
-
+import HomeTopNav from './home_top_nav';
+import BottomNav from '../shared/bottom_nav';
 
 export default function Home() {
     const dispatch = useDispatch();
-
     const currentUserId = useSelector(state => state.session.id);
     const currentUser = useSelector(state => state.entities.users[currentUserId]);
-    const userImageUrl = currentUser.image_url;
-
-
-    const currentUserShowPage = () => <div>
-        {/* <button onClick={() => dispatch(logoutUser())}>Log Out</button> */}
-        {/* <ImageForm currentUserId={currentUserId} /> */}
-        <Feed />
-        {/* <PostForm /> */}
-
-
-    </div>;
-
-    const loadingComponent = () => <div>Loading...</div>;
-
-
 
     return (
         <section>
             <HomeTopNav />
-        {
-        currentUser ? currentUserShowPage() : loadingComponent()
-        }
-
-
-        <BottomNav />
+            <button onClick={() => dispatch(logoutUser())}>Log Out</button>
+            <Feed />
+            <BottomNav />
         </section>
     );
 }
@@ -46,3 +23,5 @@ export default function Home() {
 
 
 
+
+const loadingComponent = () => <div>Loading...</div>;
