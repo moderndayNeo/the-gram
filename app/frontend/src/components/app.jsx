55437d5b1f75b4a5bb276f6308759324d0cf1417
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-15 08:09:14 +0000

    feat: Add route for Post Style

--- app/frontend/src/components/app.jsx
+++ app/frontend/src/components/app.jsx
@@ -3,14 +3,16 @@ import Home from './home/home';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import LoginPage from './session/login/login_page'
 import SignupPage from './session/signup/signup-page';
+import PostStyle from './posts/post_creation/post_style'
 
 export default function App() {
 
     return (
         <div className="app">
-            <ProtectedRoute exact path="/" component={Home} />
             <AuthRoute exact path="/signup" component={SignupPage} />
             <AuthRoute exact path="/login" component={LoginPage} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute exact path="/create/style" component={PostStyle} />
         </div>
     );
 }
