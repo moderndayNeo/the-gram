2d3b3344b5a3cdbe5b9b3cff9d69ff579ea131c3
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-15 09:50:41 +0000

    feat: Create PostStyle header

--- app/frontend/src/components/posts/post_creation/post_style.jsx
+++ app/frontend/src/components/posts/post_creation/post_style.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import {useLocation, Link} from 'react-router-dom'
+import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import icons from '../../shared/icons/svg-icons';
 
 export default function PostStyle() {
-const location = useLocation()
-console.log(location)
+    const location = useLocation();
 
     return (
-        <div>
-            {/* <img src={location.state.photoUrl} alt=""/> */}
-            Post Style
-        <Link to='/'>Back Home</Link>
+        <div className="post-style">
+            <PostStyleHeader />
+            {/* <img src={location.state.photoUrl} alt="post photo"/> */}
         </div>
-    )
+    );
 }
+
+
+const PostStyleHeader = () => (
+    <header>
+        <Link to="/">
+            {icons.cross}
+        </Link>
+        <h3>New Photo Post</h3>
+        <Link to="/create/details">
+            Next
+        </Link>
+    </header>
+);
