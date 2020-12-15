09a0f950da04e2f2c66679966f88bf30d78b4b70
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-15 17:47:26 +0000

    feat: Write out skeleton for Profile page

--- app/frontend/src/components/profile/profile.jsx
+++ app/frontend/src/components/profile/profile.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
 import BottomNav from '../shared/bottom_nav';
 
 export default function Profile() {
     return (
         <div>
-            Profile
+            <ProfileHeader />
+            <main>
+                {/* <CoreInfo /> */}
+                {/* <Stats /> */}
+                {/* <PostCollections /> */}
+            </main>
+
             <BottomNav />
         </div>
-    )
+    );
 }
+
+const ProfileHeader = () => (
+    <header>
+        Profile Header
+    </header>
+);
