677b6b43eabef2689b73446b0549413f9887d2e6
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-18 09:23:23 +0000

    refactor: Pass posts to Feed as props via Home

--- app/frontend/src/components/home/home.jsx
+++ app/frontend/src/components/home/home.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../../redux/actions/session_actions';
 import Feed from '../posts/feed';
 import HomeTopNav from './home_top_nav';
 import BottomNav from '../shared/bottom_nav';
+import {getFeed} from '../../redux/actions/post_actions'
+import stateSelectors from '../../util/state_selectors'
 
 export default function Home() {
     const dispatch = useDispatch();
+    let posts = useSelector(stateSelectors.allPosts());
+
+    if (!posts.length) dispatch(getFeed())
 
     return (
         <section className="home">
             <HomeTopNav />
             <button onClick={() => dispatch(logoutUser())}>Log Out</button>
-            <Feed />
+            <Feed posts={posts} />
             <BottomNav />
         </section>
     );
--- app/frontend/src/components/posts/feed.jsx
+++ app/frontend/src/components/posts/feed.jsx
@@ -4,32 +4,15 @@ import { getFeed } from '../../redux/actions/post_actions';
 import Post from './post';
 import stateSelectors from '../../util/state_selectors';
 
-export default function Feed() {
+export default function Feed({posts}) {
     const dispatch = useDispatch();
-    let users = useSelector(stateSelectors.allUsers());
-    let posts = useSelector(stateSelectors.allPosts());
     const [loading, setLoading] = useState(false);
     // if (!posts.length) {
     //     let retrievedObject = localStorage.getItem('developmentPosts');
     //     posts = Object.values(JSON.parse(retrievedObject))
     //     // console.log('retrievedObject: ', JSON.parse(retrievedObject));
     // }
-
-    useEffect(() => {
-        // setLoading(true);
-        dispatch(getFeed());
-
-        // setTimeout(() => {
-        //     setLoading(false);
-        // }, 1000);
-    }, []);
-
-    // useEffect(() => {
-    //     setLoading(true);
-    //     setTimeout(() => {
-    //         setLoading(false);
-    //     }, 1000);
-    // }, [posts]);
+    
 
     return (
         <ul className="feed">
