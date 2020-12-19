e2778bdad424f21f7be2e76abd1dc6e4080b1fed
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-19 05:17:09 +0000

    feat: Write comments reducer

--- app/frontend/src/redux/reducers/comments_reducer.js
+++ app/frontend/src/redux/reducers/comments_reducer.js
@@ -1,12 +1,15 @@
-import { RECEIVE_COMMENT, RECEIVE_COMMENTS } from 
+import { RECEIVE_COMMENT, RECEIVE_COMMENTS } from '../actions/comment_actions'
 
-export default (state, action) => {
+export default (state = {}, action) => {
     Object.freeze(state)
 
     switch (action.type) {
         case RECEIVE_COMMENT:
-
+            return Object.assign({}, state, {
+                [action.comment.id]: action.comment,
+            })
         case RECEIVE_COMMENTS:
+            return Object.assign({}, state, action.comments)
 
         default:
             return state
