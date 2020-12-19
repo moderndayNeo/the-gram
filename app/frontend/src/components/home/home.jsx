e0ba3910b28d9d23d2154401130da9423200896c
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-19 13:02:16 +0000

    feat: Create LoadingComponent

--- app/frontend/src/components/home/home.jsx
+++ app/frontend/src/components/home/home.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../../redux/actions/session_actions';
 import Feed from '../posts/feed';
 import HomeTopNav from './home_top_nav';
 import BottomNav from '../shared/bottom_nav';
-import {getFeed} from '../../redux/actions/post_actions'
-import stateSelectors from '../../util/state_selectors'
+import { getFeed } from '../../redux/actions/post_actions';
+import stateSelectors from '../../util/state_selectors';
 
 export default function Home() {
+    const [loading, setLoading] = useState(false);
+
     const dispatch = useDispatch();
     let posts = useSelector(stateSelectors.allPosts());
-    if (!posts.length) dispatch(getFeed())
+
+    if (!posts.length) {
+        dispatch(getFeed())
+    }
 
     return (
         <section className="home">
             <HomeTopNav />
-            <button onClick={() => dispatch(logoutUser())}>Log Out</button>
-            <Feed posts={posts} />
+            {
+                loading ?
+                    <LoadingComponent /> :
+                    <div>
+                        <button onClick={() => dispatch(logoutUser())}>Log Out</button>
+                        <Feed posts={posts} />
+                    </div>
+            }
             <BottomNav />
         </section>
     );
 }
 
-const loadingComponent = () => <div>Loading...</div>
+const LoadingComponent = () => (
+    <div className="loading-component">
+        <img src={window.cameraLoader} alt="" />
+    </div>
+);
+
+// LoadingIcon
+// LoadingSpinner
