878d0a95c69a4e9a5bf138c55c555d6febdc6fee
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-20 16:51:38 +0000

    feat: Write FollowsController create action

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -1,4 +1,20 @@
 class Api::FollowsController < ApplicationController
+  def create
+    @follow = Follow.new
+    @follow.follower = current_user
 
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+      @follow.followee = @user
+      @follow.save
+      render :show
+    elsif params[:hashtag_id]
+      # code here
+    else
+      render json: ["Error: Could not create follow"], status: 422
+    end
+  end
 
+  def destroy
+  end
 end
