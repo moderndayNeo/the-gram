81c49c3cd6e1f1ca9a984d331ac61b42ad748630
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-21 12:57:30 +0000

    feat: Create Explore SearchBar

--- app/frontend/src/components/explore/explore.jsx
+++ app/frontend/src/components/explore/explore.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from 'react';
 import BottomNav from '../shared/bottom_nav';
 import DevelopmentModal from '../shared/development_modal';
+import icons from '../shared/icons/svg-icons';
+{/* <DevelopmentModal feature={'Explore Page'} /> */ }
 
 export default function Explore() {
     return (
-        <div>
-            <DevelopmentModal feature={'Explore Page'} />
+        <div className="explore">
+            <SearchBar />
             <BottomNav />
         </div>
-    )
+    );
 }
+
+const SearchBar = () => {
+    return (
+        <header>
+            <div
+                className="search-bar search-bar-placeholder"
+            >
+
+                {icons.filledCompass}
+Search
+
+           </div>
+        </header>
+
+    );
+};
