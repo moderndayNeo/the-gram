f762faedd40e08a186a47cb15145445f4dcb2e4d
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-21 08:54:07 +0000

    feat: Use NewPostButton with camera icon

--- app/frontend/src/components/home/home_top_nav.jsx
+++ app/frontend/src/components/home/home_top_nav.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import icons from '../shared/icons/svg-icons';
 import MainLogo from '../shared/main_logo';
 import { Link } from 'react-router-dom';
+import NewPostButton from '../shared/new_post_button';
 
 export default function HomeTopNav() {
     return (
         <nav className="home-top-nav">
-            {icons.camera}
+            <NewPostButton icon={icons.camera} />
             <Link to="/">
                 <MainLogo />
             </Link>
