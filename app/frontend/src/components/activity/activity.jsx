da716e23d0708ee58e36ef5563b497bbbaf2125b
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-23 08:44:46 +0000

    feat: Display loading component when fetching notifications

--- app/frontend/src/components/activity/activity.jsx
+++ app/frontend/src/components/activity/activity.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BottomNav from '../shared/bottom_nav';
 import stateSelectors from '../../util/state_selectors';
-import {useSelector} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+import { getFeed } from '../../redux/actions/post_actions';
+import LoadingPlaceholder from '../shared/loading_placeholder'
+
 
 export default function Activity() {
+    const dispatch = useDispatch();
     const notifications = useSelector(stateSelectors.allNotifications());
-    
+
+    useEffect(() => {
+        if (!notifications.length)
+            dispatch(getFeed());
+    }, []);
 
     return (
         <div className="activity">
             <header><h3>Activity</h3></header>
 
-            <ul>
-                {
-                    notifications.map(notification => (
-                        <li key={notification.id}>
-                            <p>{notification.message}</p>
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+                !notifications.length ?
+                    <LoadingPlaceholder /> :
+                    <ul>
+                        {
+                            notifications.map(notification => (
+                                <li key={notification.id}>
+                                    <p>{notification.message}</p>
+                                </li>
+                            ))
+                        }
+                    </ul>
+            }
 
             <BottomNav />
         </div>
