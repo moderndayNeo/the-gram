af66716e1cad890f8a98b91f06256d71f1e8005a
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-23 09:47:11 +0000

    feat: Acitivty - dynamically show FollowButton or img

--- app/frontend/src/components/activity/activity.jsx
+++ app/frontend/src/components/activity/activity.jsx
@@ -45,22 +45,32 @@ const Notification = ({ notification }) => {
     const sourceUser = useSelector(stateSelectors.userById(notification.source_user_id));
     const isFollowing = useSelector(stateSelectors.currentUserIsFollowing(notification.source_user_id));
 
+    const imageOrButton = () => {
+        switch (notification.notifiable_type) {
+            case "Like":
+                const post = useSelector(stateSelectors.postById(notification.source_post_id));
+                return <SquarePostImage imageUrl={post.image_url} />;
+            case "Follow":
+                return isFollowing ?
+                    <FollowButton userId={sourceUser.id} /> : <FollowingButton userId={sourceUser.id} />;
+            default:
+                return <FollowButton userId={sourceUser.id} />;
+        }
+    };
+
     return (
         <li className="notification">
             <UserAvatar imageUrl={sourceUser.image_url} />
             <div className="details">
                 <p className="username-link">{sourceUser.username}</p>
-                <div className="info">
-                    <p>{notification.message}.</p>
-                    <p className="time-ago">12h</p>
-                    {/* <p className="time-ago">{notification.time_ago}</p> */}
-                </div>
+                <p> {notification.message}.</p>
+                <p className="time-ago"> 12h</p>
+                {/* <p className="time-ago">{notification.time_ago}</p> */}
             </div>
-            {
-                isFollowing ?
-                    <FollowButton userId={sourceUser.id} /> : <FollowingButton userId={sourceUser.id} />
-            }
+            {imageOrButton()}
         </li>
     );
 };
+
+const SquarePostImage = ({ imageUrl }) => <img src={imageUrl} alt="post" className="square-post-image" />;
 
