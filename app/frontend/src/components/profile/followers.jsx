69afd32de8bd599bb83f96ef219911d4c65e7871
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-26 15:39:54 +0000

    feat: Create Follower sub-component

--- app/frontend/src/components/profile/followers.jsx
+++ app/frontend/src/components/profile/followers.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import icons from '../shared/icons/svg-icons'
-import {useHistory} from 'react-router-dom'
+import React from 'react';
+import icons from '../shared/icons/svg-icons';
+import { useHistory } from 'react-router-dom';
+import DynamicFollowButton from '../shared/dynamic_follow_button';
 
 export default function Followers() {
-const history = useHistory()
+    const history = useHistory();
 
     return (
         <div className="followers">
             <header>
-            <div onClick={() => history.goBack()}>
+                <div onClick={() => history.goBack()}>
                     {icons.chevron}
                 </div>
                 <h3>Followers</h3>
                 <div></div>
             </header>
+
+            {/* <Follower /> */}
         </div>
-    )
+    );
 }
+
+
+// const Follower = ({ user }) => {
+//     return (
+//         <li className="follower">
+//             <div className="container">
+//                 <div className="image-and-text">
+//                     <UserAvatar imageUrl={user.image_url} />
+//                     <div className="text">
+//                         <p>emanuele calvello</p>
+//                         <p>Emanuele ;)</p>
+//                     </div>
+//                 </div>
+//                 <DynamicFollowButton userId={user.id} />
+//             </div>
+//         </li>
+//     );
+// };
--- app/frontend/src/components/shared/user_avatar.jsx
+++ app/frontend/src/components/shared/user_avatar.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-// import {useSelector} from 'react-redux'
-// import stateSelectors from '../../util/state_selectors'
 
 export default function UserAvatar({ imageUrl }) {
-    // const currentUserImg = useSelector(stateSelectors.currentUserImageUrl())
-
     return (
         <img
             className="user-avatar"
