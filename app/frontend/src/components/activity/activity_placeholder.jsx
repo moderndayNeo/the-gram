2a5695ee3bbebbead561bc420b04449cc695bc9d
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-12-31 15:17:36 +0000

    feat: Style ActivityPlaceholder

--- app/frontend/src/components/activity/activity_placeholder.jsx
+++ app/frontend/src/components/activity/activity_placeholder.jsx
@@ -4,8 +4,8 @@ import icons from '../shared/icons/svg-icons'
 export default function ActivityPlaceholder() {
     return (
         <div className="activity-placeholder">
-            <div>{icons.unfilledHeart}</div>
-            <h3>Activity On Your Posts</h3>
+            <div className="icon">{icons.unfilledHeart}</div>
+            <h2 className="title">Activity On Your Posts</h2>
             <p>When someone follows you or likes one of your posts, you'll see it here</p>
         </div>
     );
