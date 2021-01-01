6ebd9e842da15c05275e70e5d09350123ce3dc59
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-01-01 21:52:54 +0000

    feat: Install faker gem, write db seed methods

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,25 +2,29 @@ class Api::PostsController < ApplicationController
   def index
     case params[:type]
     when "feed"
-      @posts = Post
-        .where(author_id: [current_user.associated_user_ids])
-        .includes(:author, :likes, :likers)
-        .with_eager_loaded_photo
-        .newest_first
 
-      @users = User
-        .where(id: [current_user.associated_user_ids])
-        .includes(
-          :photo_attachment,
-          :saves,
-          :saved_posts,
-          :follows,
-          :liked_posts,
-          :posts,
-          :followers,
-          :followed_users,
-          :liked_comments
-        )
+      # @posts = Post
+      #   .where(author_id: [current_user.associated_user_ids])
+      #   .includes(:author, :likes, :likers)
+      #   .with_eager_loaded_photo
+      #   .newest_first
+
+      @posts = Post.all
+      @users = User.all
+
+      # @users = User
+      #   .where(id: [current_user.associated_user_ids])
+      #   .includes(
+      #     :photo_attachment,
+      #     :saves,
+      #     :saved_posts,
+      #     :follows,
+      #     :liked_posts,
+      #     :posts,
+      #     :followers,
+      #     :followed_users,
+      #     :liked_comments
+      #   )
 
       @comments = Comment
         .where(post_id: [@posts.pluck(:id)])
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,97 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+require "open-uri"
 
-u1 = User.create(
-  name: "arnie",
-  username: "arnie85",
-  bio: nil,
-  email: "arnie@example.com",
-  password: "mrolympia85",
-)
+def create_guest_account
+  User.create!(
+    id: 1,
+    name: "Guest",
+    username: "Guest",
+    bio: null,
+    email: "guest@example.com",
+    password: "guestaccount",
+  )
+end
 
-u2 = User.create(
-  name: "elon",
-  username: "elonmusk",
-  bio: nil,
-  email: "elon@example.com",
-  password: "spacex",
-)
+def create_users
+  10.times {
+    name = Faker::Name.unique.name
 
-u3 = User.create(
-  name: "omar",
-  username: "omarlittle",
-  bio: nil,
-  email: "omar@example.com",
-  password: "thewire",
-)
+    User.create!(
+      name: name,
+      username: name.split(" ").join(""),
+      bio: Faker::GreekPhilosophers.quote,
+      email: Faker::Internet.email,
+      password: name.split(" ").join(""),
+    )
+  }
+end
 
-p1 = Post.create(
-  author_id: 1,
-  caption: "Arnie post",
-)
+def create_posts
+  user_ids = User.pluck :id
 
-p2 = Post.create(
-  author_id: 1,
-  caption: "Winning mr olympia",
-)
+  3.times {
+    post = Post.new(
+      author_id: user_ids.sample,
+      caption: Faker::Quote.matz,
+      location: Faker::Address.city,
+    )
+
+    img_url = Faker::Placeholdit.image(size: "200x200", format: "png", background_color: "000000", text_color: "651fff", text: "Test")
+    img = URI.open(img_url)
+    post.photo.attach(io: img, filename: "text.png")
+    post.save!
+  }
+end
+
+# create_guest_account
+# create_posts
+# create_users
+
+def create_comments
+  post_ids = Post.pluck :id
+  user_ids = User.pluck :id
+
+  100.times {
+    Comment.create!(
+      author_id: user_ids.sample,
+      post_id: post_ids.sample,
+      body: Faker::Movies::HarryPotter.quote,
+    )
+  }
+end
+
+# create_comments
+
+# u1 = User.create(
+#   name: "arnie",
+#   username: "arnie85",
+#   bio: nil,
+#   email: "arnie@example.com",
+#   password: "mrolympia85",
+# )
+
+# u2 = User.create(
+#   name: "elon",
+#   username: "elonmusk",
+#   bio: nil,
+#   email: "elon@example.com",
+#   password: "spacex",
+# )
+
+# u3 = User.create(
+#   name: "omar",
+#   username: "omarlittle",
+#   bio: nil,
+#   email: "omar@example.com",
+#   password: "thewire",
+# )
+
+# p1 = Post.create(
+#   author_id: 1,
+#   caption: "Arnie post",
+# )
+
+# p2 = Post.create(
+#   author_id: 1,
+#   caption: "Winning mr olympia",
+# )
