7fe223480949a6d67644b42ed049776642bf9e93
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-01-08 08:37:06 +0000

    refactor: Store current_user with suggested_users

--- app/frontend/src/redux/actions/user_actions.js
+++ app/frontend/src/redux/actions/user_actions.js
@@ -1,17 +1,26 @@
+import { batch } from 'react-redux'
 import * as APIUtil from '../../util/api_util'
 import { receiveUsers, receiveCurrentUser } from './session_actions'
 export const RECEIVE_USER_ERRORS = 'RECEIVE_USER_ERRORS'
 export const RECEIVE_USER = 'RECEIVE_USER'
 
 export const fetchFollowers = () => (dispatch) =>
-    APIUtil.fetchFollowers().then(({ data: { users } }) =>
-        dispatch(receiveUsers(users))
-    )
+    APIUtil.fetchFollowers().then(({ data: { users, current_user } }) => {
+        batch(() => {
+            dispatch(receiveCurrentUser(current_user))
+            dispatch(receiveUsers(users))
+        })
+    })
 
 export const fetchSuggestedUsers = () => (dispatch) =>
-    APIUtil.fetchUsersNotFollowed().then(({ data: { users } }) => {
-        dispatch(receiveUsers(users))
-    })
+    APIUtil.fetchUsersNotFollowed().then(
+        ({ data: { users, current_user } }) => {
+            batch(() => {
+                dispatch(receiveUsers(users))
+                dispatch(receiveCurrentUser(current_user))
+            })
+        }
+    )
 
 export const updateUser = (userId, formData) => (dispatch) =>
     APIUtil.updateUser(userId, formData)
@@ -27,4 +36,3 @@ const receiveUserErrors = (errors) => ({
     type: RECEIVE_USER_ERRORS,
     errors,
 })
-
