3cb219731d8622dcea0a534d9c093f9467bc1492
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-01-08 10:32:19 +0000

    refactor: Fetch current_user private info on refresh

---
+++ app/frontend/src/components/home/new_user_homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NewUserHomepage() {
+    return (
+        <div className="new-user-homepage">
+            <h3>Welcome to Instagram</h3>
+        </div>
+    );
+}
