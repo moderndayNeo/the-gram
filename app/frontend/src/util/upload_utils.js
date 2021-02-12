2a1cbcfbcf2f5f19c8a975d982884b3fedebd697
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-12 13:11:54 +0000

    refactor: Refactor dataURItoBlob function

--- app/frontend/src/util/upload_utils.js
+++ app/frontend/src/util/upload_utils.js
@@ -61,17 +61,17 @@ export function createFileWithImage(img) {
 
 export function dataURItoBlob(dataURI) {
     // convert base64/URLEncoded data component to raw binary data held in a string
-    var byteString
+    let byteString
     if (dataURI.split(',')[0].indexOf('base64') >= 0)
         byteString = atob(dataURI.split(',')[1])
     else byteString = unescape(dataURI.split(',')[1])
 
     // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+    let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
 
     // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length)
-    for (var i = 0; i < byteString.length; i++) {
+    let ia = new Uint8Array(byteString.length)
+    for (let i = 0; i < byteString.length; i++) {
         ia[i] = byteString.charCodeAt(i)
     }
 
